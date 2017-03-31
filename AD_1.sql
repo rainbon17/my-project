5d089efdd3735999f64150c943a4c83c913e259c
rainbon17 <rainbon17@outlook.com>
2017-03-31 08:55:35 +0800

    Add files via upload

--- AD_1.sql
+++ AD_1.sql
@@ -4,16 +4,19 @@ select * from sysibm.systables
 where 1=1 and creator = 'DBAD' 
 and type = 'T'
 
-
->java -jar schemaSpy_5.0.0.jar -t db2jcc -db insdb -u DBAD -p dbad88 -schema users -o d:/dbad88
+set PATH=D:\Tools\java832\bin;%PATH%
+>java -jar schemaSpy 5.0.0.jar -t db2jcc -db insdb -u DBAD -p dbad88 -schema users -charset UTF-8 -o d:/dbad88
 SchemaSpy生成的文?默?是ISO-8859-1,中文?示就是??,需要制定??-charset UTF-8.?异的是schemaspy的help和主?上的用法都?有提到?一?,俺是在源?里找到????的.原??就是?源的好?啊.
+java -jar schemaspy-6.0.0-beta.3.jar -t db2jcc -db insdb -u DBAB -p dbab88 -schema users  -charset UTF-8 -o d:/dbab88b
+java -jar schemaspy_6.0.0.jar -t db2jcc -dp 'c:/Program Files/IBM/SQLLIB/java/db2jcc4.jar' -db insdb  -schema users  -u DBDJ -p dbdj88 -o d:/dbdj88d    -gv D:\Tools\Graphviz2x.7\Graphviz2.38
+java -jar schemaSpy_5.0.0.jar -t db2jcc -db insdb -u DBAD -p dbad88 -schema users -charset UTF-8 -o d:/dbad88c -gv D:\Tools\Graphviz2x.7\Graphviz2.38
 
 --經手人檔DTADZ060  --經手人招攬資格檔DTADZ061
-Select ma.*,se.GATHER_CERTIFY from DBAD.DTADZ060 ma ,DTADZ061 se where ma.AGENT_ID=se.AGENT_ID
+Select ma.*,se.GATHER CERTIFY from DBAD.DTADZ060 ma ,DTADZ061 se where ma.AGENT ID=se.AGENT ID
 月玲 N221280655 
 謝淑錦 N222227821
 --出單歸屬單位檔DTADZ070
-Select zs.div_name,zs.COUNSEL_ID from DBAD.DTADZ060 ma ,DTADZ070 zs where ma.AGENT_DIV_NO=zs.DIV_NO and   ma.AGENT_NAME like '%月玲%'
+Select zs.div name,zs.COUNSEL ID from DBAD.DTADZ060 ma ,DTADZ070 zs where ma.AGENT DIV NO=zs.DIV NO and   ma.AGENT NAME like '%月玲%'
 
 
 --保經代據點分支檔DTADZ052
@@ -21,14 +24,63 @@ DTADZ056
 
 --##務費用代號查詢##
 Select 
-APPLY_NO,APPLY_ID,
-BROKER_NO||'- '||(select COMPANY_FULL_NAME from DTADZ056 zf where zf.BROKER_NO=zs.BROKER_NO )BROKER_NO,--經紀人代碼
-zs.PRODUCT_CODE||'- '||(select Field_Name from DBAG.DTAGZ020 gz where gz.database_no='AT' and gz.field_no='PRODUCT_CODE' and gz.field_contain=zs.PRODUCT_CODE)PRODUCT_CODE,--商品類別代號
-zs.BUSINESS_FEE_TYPE||'- '||(select Field_Name from DBAG.DTAGZ020 gz where gz.database_no='AD' and gz.field_no='SC_BUSINESS_FEE_TYPE' and gz.field_contain=zs.BUSINESS_FEE_TYPE)BUSINESS_FEE_TYPE,--業務費用類型
-zs.APPLY_TYPE||'- '||(select Field_Name from DBAG.DTAGZ020 gz where gz.database_no='AD' and gz.field_no='SC_APPLY_TYPE' and gz.field_contain=zs.APPLY_TYPE)APPLY_TYPE,--申請類型
-zs.APPLY_DIV_NO||'- '||(select DIV_NAME from DTADZ070 dz where dz.DIV_NO=zs.APPLY_DIV_NO)APPLY_DIV_NO,--申請單位
-COPERATE_COMPANY_NO  from DBAD.DTADZ770 zs --業務費用代號
+APPLY NO,APPLY ID,
+BROKER NO||'- '||(select COMPANY FULL NAME from DTADZ056 zf where zf.BROKER NO=zs.BROKER NO )BROKER NO,--經紀人代碼
+zs.PRODUCT CODE||'- '||(select Field Name from DBAG.DTAGZ020 gz where gz.database no='AT' and gz.field no='PRODUCT CODE' and gz.field contain=zs.PRODUCT CODE)PRODUCT CODE,--商品類別代號
+zs.BUSINESS FEE TYPE||'- '||(select Field Name from DBAG.DTAGZ020 gz where gz.database no='AD' and gz.field no='SC BUSINESS FEE TYPE' and gz.field contain=zs.BUSINESS FEE TYPE)BUSINESS FEE TYPE,--業務費用類型
+zs.APPLY TYPE||'- '||(select Field Name from DBAG.DTAGZ020 gz where gz.database no='AD' and gz.field no='SC APPLY TYPE' and gz.field contain=zs.APPLY TYPE)APPLY TYPE,--申請類型
+zs.APPLY DIV NO||'- '||(select DIV NAME from DTADZ070 dz where dz.DIV NO=zs.APPLY DIV NO)APPLY DIV NO,--申請單位
+COPERATE COMPANY NO  from DBAD.DTADZ770 zs --業務費用代號
 where  1=1 and
-COPERATE_COMPANY_NO='BEAF'
---apply_no='CSC0000012'
+COPERATE COMPANY NO='BEAF'
+--apply no='CSC0000012'
 --ORDER BY
+
+
+--業佣率修正作業 
+Select * from DBAD.DTADE000 where policy_no='150106BO100003'
+Select * from DBAD.DTADE101 where policy_no='150106BO100003'
+
+update DBAD.DTADE101 set status='5' where status='1' and policy_no='150106BO100003' //enbable btn:修改
+
+
+//********************************************************************************************************
+
+
+
+
+//********************************************************************************************************
+select count(name),creator from sysibm.systables 
+where type='T'
+group by creator having creator like 'DB%'
+1,CREATOR
+'16','DB2INST1'
+'17','DB2ADMIN'
+'17','DBAM'
+'23','DBZE'
+'26','DBBB'
+'28','DBDS'
+'29','DBED'
+'41','DBDF'
+'43','DBMG'
+'50','DBFW'
+'62','DBDD'
+'69','DBEA'
+'71','DBDE'
+'96','DBDC'
+'123','DBPA'
+'125','DBDJ'
+'142','DBCM'
+'166','DBAE'
+'182','DBAG'
+'212','DBZZ'
+'246','DBDK'
+'252','DBAR'
+'436','DBRP'
+'457','DBHR'
+'588','DBAA'
+'687','DBAD'
+'702','DBFA'
+'822','DBAB'
+'825','DBBP'
+'1082','DBAT'
